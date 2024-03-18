f8fc022a887e7ad855ecab0ecfca5471ccbb4e78
agwhite2004 <agw00027@mix.wvu.edu>
2024-03-17 23:17:59 -0400

    Library App Done

---
+++ frontend/src/pages/Add.jsx
@@ -0,0 +1,42 @@
+import React from "react"
+import { useNavigate } from "react-router-dom"
+import { useState } from "react"
+import axios from "axios"
+
+const Add = () => {
+    const [book, setBook] = useState({
+        title: "",
+        desc: "",
+        price: null,
+        cover: ""
+    })
+
+    const navigate = useNavigate()
+
+    const handleChange = (e) => {
+        setBook((prev) => ({...prev, [e.target.name] : e.target.value}))
+    }
+
+    const handleClick = async e => {
+        e.preventDefault()
+        try{
+            await axios.post("http://localhost:8800/books", book)
+            navigate("/")
+        }catch(err){
+            console.log(err)
+        }
+    }
+
+    return(
+        <div className = "form">
+            <h1>Add New Book</h1>
+            <input type = "text" placeholder = "title" onChange = {handleChange} name = "title"></input>
+            <input type = "text" placeholder = "desc" onChange = {handleChange} name = "desc"></input>
+            <input type = "number" placeholder = "price" onChange = {handleChange} name = "price"></input>
+            <input type = "text" placeholder = "cover" onChange = {handleChange} name = "cover"></input>
+            <button className = "formButton" onClick = {handleClick}>Add</button>
+        </div>
+    )
+}
+
+export default Add;
